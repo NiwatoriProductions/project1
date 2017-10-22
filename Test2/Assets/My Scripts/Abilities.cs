5c61bf5e67e20b9c816e6e2cf275ef64d6712fcc
Justin Veyna <jcveyna@uci.edu>
2017-10-21 17:33:46 -0700

    Testing out (Do not touch- except Carolyn may play around with the animations)
    Code needs major cleaning up but for now 2 abilities and the base layer
    swaping mechanics are in
    e-fly (nulifies gravity for a bit, only in spiritual layer)
    shift-swap layers
    space-jump and air jump(in spiritual layer)

---
+++ Test2/Assets/My Scripts/Abilities.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MyScripts;
+
+class m_Action
+{
+
+}
+public class Abilities : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        List<m_Action> actions = new List<m_Action>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
